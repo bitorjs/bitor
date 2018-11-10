d71f57dcbd83b5ff4c9cb386158ef9fc3f6dbf71
bitores <773155801@qq.com>
2018-11-10 11:12:35 +0800

    fix bug

--- index.js
+++ index.js
@@ -2,4 +2,18 @@ import HashHistory from "../HashHistory/index";
 
 console.log(HashHistory)
 
-window.hash = HashHistory.Hash;
+let mode = 'h5'; //'hash'; //
+let current = null;
+switch (mode) {
+  case 'hash':
+    current = new HashHistory.Hash().listen();
+    break;
+  case 'h5':
+    current = new HashHistory.History().listen();
+    break;
+
+  default:
+    break;
+}
+
+window.hash = current;
